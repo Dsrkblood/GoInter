f95260820e854c53cdfa513cfd67f9e9cfff239c
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-02-04 19:14:42 +0100

    Mott.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
-import Particles from "react-tsparticles";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ParticlesAnimation from "./components/ParticlesAnimation";
+import Motto from "./components/Motto";
 
 function App() {
 	return (
 		<>
-			{/* <Navbar /> */}
 			<div id='hero-bg'>
 				<ParticlesAnimation />
 			</div>
-
-			<div className='info'>
-				<div className='mott-text'>
-					<p>internet</p>
-					<p>na</p>
-					<p>każdą</p>
-					<p>kieszeń</p>
-				</div>
-				<div className='social'>
-					<p className='social-heading'>media</p>
-					<div className='social-icons'>
-						<a href='#'>
-							<i className='fa-brands fa-facebook-f'></i>
-						</a>
-						<a href='#'>
-							<i className='fa-brands fa-linkedin-in'></i>
-						</a>
-						<a href='#'>
-							<i className='fa-brands fa-x-twitter'></i>
-						</a>
-					</div>
-				</div>
-			</div>
+			<Navbar />
+			<Motto />
 		</>
 	);
 }
---
+++ src/components/Motto.jsx
@@ -0,0 +1,26 @@
+export default function Motto() {
+	return (
+		<div className='info'>
+			<div className='mott-text'>
+				<p>internet</p>
+				<p>na</p>
+				<p>każdą</p>
+				<p>kieszeń</p>
+			</div>
+			<div className='social'>
+				<p className='social-heading'>media </p>
+				<div className='social-icons'>
+					<a href='#'>
+						<i className='fa-brands fa-facebook-f'></i>
+					</a>
+					<a href='#'>
+						<i className='fa-brands fa-linkedin-in'></i>
+					</a>
+					<a href='#'>
+						<i className='fa-brands fa-x-twitter'></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
