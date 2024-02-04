55525012f03d0ca9dff85b6829db13f2a2288a99
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-02-04 17:32:25 +0100

    Add Particle animation like hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import Particles from "react-tsparticles";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import ParticleBg from "./components/ParticleBg";
+import ParticlesAnimation from "./components/ParticlesAnimation";
 
 function App() {
 	return (
 		<>
 			{/* <Navbar /> */}
-			<ParticleBg />
+			<div id='hero-bg'>
+				<ParticlesAnimation />
+			</div>
+
+			<div className='info'>
+				<div className='mott-text'>
+					<span>internet</span>
+					<span>na</span>
+					<span>każdą</span>
+					<span>kieszeń</span>
+				</div>
+				<div className='social'>
+					<span>media</span>
+					<a href='#'>
+						<i className='fa-brands fa-facebook-f'></i>
+					</a>
+					<a href='#'>
+						<i className='fa-brands fa-linkedin-in'></i>
+					</a>
+					<a href='#'>
+						<i className='fa-brands fa-x-twitter'></i>
+					</a>
+				</div>
+			</div>
 		</>
 	);
 }
---
+++ src/components/ParticlesAnimation.jsx
@@ -0,0 +1,92 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+export default function ParticlesAnimation() {
+	const init = useCallback(async engine => {
+		await loadSlim(engine);
+	}, []);
+	return (
+		<Particles
+			className='particles'
+			options={{
+				fullScreen: {
+					enable: false,
+				},
+				fpsLimit: 120,
+				interactivity: {
+					events: {
+						onClick: {
+							enable: true,
+							mode: "push",
+						},
+						onDiv: {
+							enable: false,
+							type: "circle",
+						},
+						onHover: {
+							enable: true,
+							mode: "slow",
+							parallax: {
+								enable: false,
+								force: 60,
+								smooth: 10,
+							},
+						},
+						resize: {
+							delay: 0.5,
+							enable: true,
+						},
+					},
+					modes: {
+						push: {
+							quantity: 2,
+						},
+						repulse: {
+							distance: 100,
+							duration: 0.4,
+						},
+					},
+				},
+				particles: {
+					color: {
+						value: "#fff",
+					},
+					links: {
+						color: "#fff",
+						distance: 150,
+						enable: true,
+						opacity: 0.5,
+						width: 1,
+					},
+					move: {
+						direction: "none",
+						enable: true,
+						outModes: {
+							default: "out",
+						},
+						random: false,
+						speed: 10,
+						straight: false,
+					},
+					number: {
+						density: {
+							enable: true,
+						},
+						value: 100,
+					},
+					opacity: {
+						value: 0.5,
+					},
+					shape: {
+						type: "circle",
+					},
+					size: {
+						value: { min: 1, max: 5 },
+					},
+				},
+				detectRetina: true,
+			}}
+			init={init}
+		/>
+	);
+}
