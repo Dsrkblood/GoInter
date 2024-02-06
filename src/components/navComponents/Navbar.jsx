7d9f040efd67a195e639bf1b784436fa1f0cb314
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-02-06 19:56:15 +0100

    Navigation for mobile and desktop

--- src/components/navComponents/Navbar.jsx
+++ src/components/navComponents/Navbar.jsx
@@ -25,9 +25,19 @@ export default function Navbar() {
 	return (
 		<nav id='nav' className={isNavScrolled ? "active" : null}>
 			<p className='company_name'>gointer</p>
-			<button onClick={handleOpenMenu} className='menu'>
-				<i className='fa-solid fa-bars'></i>
-			</button>
+			<div className='mobile'>
+				<button onClick={handleOpenMenu} className='menu'>
+					<i className='fa-solid fa-bars'></i>
+				</button>
+			</div>
+			<div className='desktop'>
+				<a href='#'>start</a>
+				<a href='#'>internet</a>
+				<a href='#'>telefon</a>
+				<a href='#'>telewizja</a>
+				<a href='#'>monitoring</a>
+				<i className='fa-solid fa-ellipsis'></i>
+			</div>
 			<Menu ref={modal} buttonCaption='Close' />
 		</nav>
 	);
