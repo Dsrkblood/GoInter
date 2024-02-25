7f358b5558488c1e816540c78bb0c98641233baf
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-02-25 21:19:17 +0100

    error correction

--- src/components/headerComponents/promoContent/SocialMedia.jsx
+++ src/components/headerComponents/promoContent/SocialMedia.jsx
@@ -7,11 +7,11 @@ export default function SocialMedia() {
 					<a href='#' className='facebook' aria-label='Facebook'>
 						<i className='fa-brands fa-facebook-f'></i>
 					</a>
-					<a href='#' className='linkedin'>
-						<i className='fa-brands fa-linkedin-in' aria-label='Linkedin'></i>
+					<a href='#' className='linkedin' aria-label='Linkedin'>
+						<i className='fa-brands fa-linkedin-in'></i>
 					</a>
-					<a href='#' className='twitter'>
-						<i className='fa-brands fa-x-twitter' aria-label='Twitter'></i>
+					<a href='#' className='twitter' aria-label='Twitter'>
+						<i className='fa-brands fa-x-twitter'></i>
 					</a>
 				</div>
 			</div>
