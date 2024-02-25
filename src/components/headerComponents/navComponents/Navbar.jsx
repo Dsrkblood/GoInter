f629e5671a9f92e8433e071f2f69da7fde1e5307
Przemysław Żołądek <przemyslawzoladek1302@gmail.com>
2024-02-25 21:11:22 +0100

    improvements for screen reader support

--- src/components/headerComponents/navComponents/Navbar.jsx
+++ src/components/headerComponents/navComponents/Navbar.jsx
@@ -6,6 +6,7 @@ import { CONTENT } from "../../../data";
 export default function Navbar({ changeContent }) {
 	const [isNavScrolled, setIsNavScrolled] = useState(false);
 	const [isOpen, setIsOpen] = useState(false);
+	const [openPopup, setOpenPopup] = useState(false);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -22,6 +23,12 @@ export default function Navbar({ changeContent }) {
 	function handleOpen() {
 		setIsOpen(prevOpen => !prevOpen);
 	}
+	function handleClosePopup(value) {
+		setOpenPopup(prevPopup => (prevPopup = value));
+	}
+	function handleOpenPopup() {
+		setOpenPopup(prevPopup => !prevPopup);
+	}
 
 	return (
 		<>
@@ -29,6 +36,7 @@ export default function Navbar({ changeContent }) {
 				<p className='highlighted-text'>gointer</p>
 				<div className='mobile'>
 					<button
+						aria-label='Nawigacja'
 						onClick={handleOpen}
 						className={`icon ${isOpen ? "active" : null}`}>
 						<div className='bar'></div>
@@ -54,19 +62,38 @@ export default function Navbar({ changeContent }) {
 					<a href='#top' onClick={() => changeContent(CONTENT.monitoring)}>
 						monitoring
 					</a>
-					<div className='container'>
-						<i className='fa-solid fa-ellipsis'></i>
-						<div className='popup'>
-							<a href='#top' onClick={() => changeContent(CONTENT.promotions)}>
+					<div className='container' aria-label='Rozwiń'>
+						<a href='#' onClick={handleOpenPopup}>
+							<i className='fa-solid fa-ellipsis'></i>
+						</a>
+
+						<div className={`popup ${openPopup ? "open" : ""}`}>
+							<a
+								href='#top'
+								onClick={() => {
+									changeContent(CONTENT.promotions), handleClosePopup(false);
+								}}>
 								promocje
 							</a>
-							<a href='#top' onClick={() => changeContent(CONTENT.order)}>
+							<a
+								href='#top'
+								onClick={() => {
+									changeContent(CONTENT.order), handleClosePopup(false);
+								}}>
 								zamów usługę
 							</a>
-							<a href='#top' onClick={() => changeContent(CONTENT.aboutus)}>
+							<a
+								href='#top'
+								onClick={() => {
+									changeContent(CONTENT.aboutus), handleClosePopup(false);
+								}}>
 								o nas
 							</a>
-							<a href='#top' onClick={() => changeContent(CONTENT.contact)}>
+							<a
+								href='#top'
+								onClick={() => {
+									changeContent(CONTENT.contact), handleClosePopup(false);
+								}}>
 								kontakt
 							</a>
 						</div>
--- src/components/headerComponents/promoContent/SocialMedia.jsx
+++ src/components/headerComponents/promoContent/SocialMedia.jsx
@@ -4,14 +4,14 @@ export default function SocialMedia() {
 			<div className='social'>
 				<p className='social-heading'>media </p>
 				<div className='social-icons'>
-					<a href='#' className='facebook'>
+					<a href='#' className='facebook' aria-label='Facebook'>
 						<i className='fa-brands fa-facebook-f'></i>
 					</a>
 					<a href='#' className='linkedin'>
-						<i className='fa-brands fa-linkedin-in'></i>
+						<i className='fa-brands fa-linkedin-in' aria-label='Linkedin'></i>
 					</a>
 					<a href='#' className='twitter'>
-						<i className='fa-brands fa-x-twitter'></i>
+						<i className='fa-brands fa-x-twitter' aria-label='Twitter'></i>
 					</a>
 				</div>
 			</div>
